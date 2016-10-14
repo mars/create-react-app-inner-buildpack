127d4edb9ea2bfec634429acab9dcfaba9369e82
Mars Hall <mars@heroku.com>
2016-10-14 14:07:05 -0700

    Escape ampersand too; they break sed expression

--- .profile.d/inject_react_app_env.sh
+++ .profile.d/inject_react_app_env.sh
@@ -9,7 +9,7 @@ set -e
 vars_as_json=`ruby -E utf-8:utf-8 -r /app/.heroku/create-react-app/injectable_env.rb -e InjectableEnv.render`
 # Add another level of escaped-backslashes, so we still have 
 # escaped values after sed command, which consumes a level of escapes.
-escaped_vars_as_json=$(echo $vars_as_json | sed 's/\\/\\\\/g' | sed 's/\//\\\//g')
+escaped_vars_as_json=$(echo $vars_as_json | sed 's/\\/\\\\/g' | sed 's/\//\\\//g' | sed 's/\&/\\\&/g')
 
 # Each bundle is generated with a unique hash name to bust browser cache.
 js_bundle=`ls /app/build/static/js/main.*.js`
