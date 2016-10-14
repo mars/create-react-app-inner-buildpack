9b17224abaa94972fc76b96d529222b49d71b787
Mars Hall <mars@heroku.com>
2016-10-14 11:58:34 -0700

    Fix runtime paths

--- .profile.d/inject_react_app_env.sh
+++ .profile.d/inject_react_app_env.sh
@@ -6,10 +6,10 @@ set -e
 #set -x
 
 # Render runtime env vars into injectable JSON.
-vars_as_json=`ruby -r .heroku/create-react-app/injectable_env.rb -e InjectableEnv.render`
+vars_as_json=`ruby -r /app/.heroku/create-react-app/injectable_env.rb -e InjectableEnv.render`
 
 # Each bundle is generated with a unique hash name to bust browser cache.
-js_bundle=`ls build/static/js/main.*.js`
+js_bundle=`ls /app/build/static/js/main.*.js`
 
 # Inject the escaped JSON into the Webpack bundle.
 sed s/{{REACT_APP_VARS_AS_JSON}}/${vars_as_json}/ $js_bundle
