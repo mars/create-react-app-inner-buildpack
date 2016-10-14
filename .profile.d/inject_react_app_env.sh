cc46aa0f51f0e546d5552f41c9098a525252d324
Mars Hall <mars@heroku.com>
2016-10-14 12:41:29 -0700

    Another level of escapes.

--- .profile.d/inject_react_app_env.sh
+++ .profile.d/inject_react_app_env.sh
@@ -7,10 +7,12 @@ set -e
 
 # Render runtime env vars into injectable JSON.
 vars_as_json=`ruby -E utf-8:utf-8 -r /app/.heroku/create-react-app/injectable_env.rb -e InjectableEnv.render`
+# Add another level of escaped-backslashes, so we still have 
+# escaped values after sed command, which consumes a level of escapes.
+escaped_vars_as_json=$(echo $vars_as_json | sed 's/\\/\\\\/g')
 
 # Each bundle is generated with a unique hash name to bust browser cache.
 js_bundle=`ls /app/build/static/js/main.*.js`
 
 # Inject the escaped JSON into the Webpack bundle.
-runtime_bundle=`sed s/{{REACT_APP_VARS_AS_JSON}}/${vars_as_json}/ $js_bundle`
-echo $runtime_bundle > $js_bundle
+sed --in-place=.orig s/{{REACT_APP_VARS_AS_JSON}}/${escaped_vars_as_json}/ $js_bundle
