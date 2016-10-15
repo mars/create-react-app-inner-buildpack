95bcc21b472bec2eee7a03a8b3af30b5b69c3b78
Mars Hall <mars@heroku.com>
2016-10-14 17:03:04 -0700

    Fix file path to JS bundle

--- .profile.d/inject_react_app_env.sh
+++ .profile.d/inject_react_app_env.sh
@@ -11,4 +11,4 @@ js_bundle=`ls /app/build/static/js/main.*.js`
 # Render runtime env vars into bundle.
 ruby -E utf-8:utf-8 \
   -r /app/.heroku/create-react-app/injectable_env.rb \
-  -e "InjectableEnv.replace('/app/build/static/js/$js_bundle')"
+  -e "InjectableEnv.replace('$js_bundle')"
