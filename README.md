7c02a5954f83ed4c21d7704591687475ca515d0a
Mars Hall <mars@users.noreply.github.com>
2016-10-16 17:41:28 -0700

    Modular runtime env vars (#3)
    * Set-up env var replacement in generated index file
    
    * Render Mustaches in the Webpack bundle
    
    * Fix bundle name
    
    * Generate composite `REACT_APP_VARS_AS_JSON` env var at runtime
    
    * Add runtime config to slug
    
    * Fix compile path
    
    * Escape quotes so bundle replacement works
    
    * Set utf-8 for JSON encoding function, because old-skool Ruby 1.9
    
    * Switch to Ruby script for env var to JSON conversion.
    
    * Fix runtime paths
    
    * Set utf-8 for JSON encoding function, because old-skool Ruby 1.9
    
    * Fix for env values with unknown encoding.
    
    * Actually write the runtime bundle
    
    * Another level of escapes.
    
    * Fix for space char breaking sed expression
    
    * Escape forward slashes too; they break sed expression
    
    * Escape ampersand too; they break sed expression
    
    * Replace `sed` JSON injection with pure Ruby
    
    * Fix pure Ruby injector command with correct args
    
    * Fix file path to JS bundle
    
    * More escapes for JSON values
    
    * Fix so injected values just work without further escaping by developer.
    
    * TravisCI
    
    * Use rake to execute tests (for TravisCI)
    
    * Fix missing dependency
    
    * Simplification fix for double quote escaping.
    
    * Triple backslash escape for double-quote in JSON value.
    
    * Revise JSON escaping for control chars
    
    * Fail gracefully for old CRA versions
    
    * Improve "Injecting runtime" log message

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ Inner layer of Heroku Buildpack for create-react-app
 ====================================================
 
 See: [create-react-app-buildpack](https://github.com/mars/create-react-app-buildpack)
+
+[![Build Status](https://travis-ci.org/mars/create-react-app-inner-buildpack.svg?branch=master)](https://travis-ci.org/mars/create-react-app-inner-buildpack)
+
+Development
+-----------
+
+Use Ruby 1.9.3 as built-in to Cedar-14.
+
+Run tests: `bundle exec rake`
---
+++ lib/injectable_env.rb
@@ -0,0 +1,51 @@
+# encoding: utf-8
+require 'json'
+
+class InjectableEnv
+  DefaultVarMatcher = /^REACT_APP_/
+  Placeholder='{{REACT_APP_VARS_AS_JSON}}'
+
+  def self.create(var_matcher=DefaultVarMatcher)
+    vars = ENV.find_all {|name,value| var_matcher===name }
+
+    json = '{'
+    is_first = true
+    vars.each do |name,value|
+      json += ',' unless is_first
+      json += "#{escape(name)}:#{escape(value)}"
+      is_first = false
+    end
+    json += '}'
+  end
+
+  def self.render(*args)
+    $stdout.write create(*args)
+    $stdout.flush
+  end
+
+  def self.replace(file, *args)
+    injectee = IO.read(file)
+    return unless injectee.index(Placeholder)
+
+    env = create(*args)
+    head,_,tail = injectee.partition(Placeholder)
+    injected = head + env + tail
+    File.open(file, 'w') do |f|
+      f.write(injected)
+    end
+  end
+
+  # Escape JSON name/value double-quotes so payload can be injected 
+  # into Webpack bundle where embedded in a double-quoted string.
+  #
+  def self.escape(v)
+    v.dup
+      .force_encoding('utf-8') # UTF-8 encoding for content
+      .to_json
+      .gsub(/\\\\/, '\\\\\\\\\\\\\\\\') # single slash in content
+      .gsub(/\\([bfnrt])/, '\\\\\\\\\1') # control sequence in content
+      .gsub(/([^\A])\"([^\Z])/, '\1\\\\\\"\2') # double-quote in content
+      .gsub(/(\A\"|\"\Z)/, '\\\"') # double-quote around JSON token
+  end
+
+end
